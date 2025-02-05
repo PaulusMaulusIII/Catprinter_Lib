fc008e5725d814aefa9727c0c61185f4b5eab90b
Paul Schröder <paulschroeder05@outlook.de>
2025-02-05 19:46:06 +0100

    Removed dynamic typing

--- PrinterCommands.cs
+++ PrinterCommands.cs
@@ -10,8 +10,8 @@ namespace Catprinter.Utils
             byte[] data = new byte[0];
             data = AddToByteArray(data, CMD_GET_DEV_STATE);
             data = AddToByteArray(data, CMD_SET_QUALITY_200_DPI);
-            data = AddToByteArray(data, CmdSetEnergy(0xffff));
-            data = AddToByteArray(data, CmdApplyEnergy());
+            data = AddToByteArray(data, GenerateSetEnergyCommand(0xffff));
+            data = AddToByteArray(data, GenerateApplyEnergyCommand());
             data = AddToByteArray(data, CMD_LATTICE_START);
 
             for (int y = 0; y < img.Height; y++)
@@ -21,10 +21,10 @@ namespace Catprinter.Utils
                 {
                     row.Add(img.GetPixel(x, y).ToArgb() & 0xFF);
                 }
-                data = AddToByteArray(data, CmdPrintRow(row));
+                data = AddToByteArray(data, GeneratePrintRowCommand(row));
             }
 
-            data = AddToByteArray(data, CmdFeedPaper(25));
+            data = AddToByteArray(data, GenerateFeedPaperCommand(25));
             data = AddToByteArray(data, CMD_SET_PAPER);
             data = AddToByteArray(data, CMD_SET_PAPER);
             data = AddToByteArray(data, CMD_LATTICE_END);
@@ -60,21 +60,21 @@ namespace Catprinter.Utils
             return (byte)(val >= 0 ? val : val & 0xff);
         }
 
-        private static byte[] Bs(IEnumerable<int> lst)
+        private static byte[] ConvertToUByteArray(IEnumerable<int> lst)
         {
             return lst.Select(val => (byte)(val >= 0 ? val : val + 256)).ToArray();
         }
 
-        private static readonly byte[] CMD_GET_DEV_STATE = Bs(new[] { 81, 120, -93, 0, 1, 0, 0, 0, -1 });
-        private static readonly byte[] CMD_SET_QUALITY_200_DPI = Bs(new[] { 81, 120, -92, 0, 1, 0, 50, -98, -1 });
-        private static readonly byte[] CMD_GET_DEV_INFO = Bs(new[] { 81, 120, -88, 0, 1, 0, 0, 0, -1 });
-        private static readonly byte[] CMD_LATTICE_START = Bs(new[] { 81, 120, -90, 0, 11, 0, -86, 85, 23, 56, 68, 95, 95, 95, 68, 56, 44, -95, -1 });
-        private static readonly byte[] CMD_LATTICE_END = Bs(new[] { 81, 120, -90, 0, 11, 0, -86, 85, 23, 0, 0, 0, 0, 0, 0, 0, 23, 17, -1 });
-        private static readonly byte[] CMD_SET_PAPER = Bs(new[] { 81, 120, -95, 0, 2, 0, 48, 0, -7, -1 });
-        private static readonly byte[] CMD_PRINT_IMG = Bs(new[] { 81, 120, -66, 0, 1, 0, 0, 0, -1 });
-        private static readonly byte[] CMD_PRINT_TEXT = Bs(new[] { 81, 120, -66, 0, 1, 0, 1, 7, -1 });
+        private static readonly byte[] CMD_GET_DEV_STATE = ConvertToUByteArray(new[] { 81, 120, -93, 0, 1, 0, 0, 0, -1 });
+        private static readonly byte[] CMD_SET_QUALITY_200_DPI = ConvertToUByteArray(new[] { 81, 120, -92, 0, 1, 0, 50, -98, -1 });
+        private static readonly byte[] CMD_GET_DEV_INFO = ConvertToUByteArray(new[] { 81, 120, -88, 0, 1, 0, 0, 0, -1 });
+        private static readonly byte[] CMD_LATTICE_START = ConvertToUByteArray(new[] { 81, 120, -90, 0, 11, 0, -86, 85, 23, 56, 68, 95, 95, 95, 68, 56, 44, -95, -1 });
+        private static readonly byte[] CMD_LATTICE_END = ConvertToUByteArray(new[] { 81, 120, -90, 0, 11, 0, -86, 85, 23, 0, 0, 0, 0, 0, 0, 0, 23, 17, -1 });
+        private static readonly byte[] CMD_SET_PAPER = ConvertToUByteArray(new[] { 81, 120, -95, 0, 2, 0, 48, 0, -7, -1 });
+        private static readonly byte[] CMD_PRINT_IMG = ConvertToUByteArray(new[] { 81, 120, -66, 0, 1, 0, 0, 0, -1 });
+        private static readonly byte[] CMD_PRINT_TEXT = ConvertToUByteArray(new[] { 81, 120, -66, 0, 1, 0, 1, 7, -1 });
 
-        private static readonly byte[] CHECKSUM_TABLE = Bs(new[]
+        private static readonly byte[] CHECKSUM = ConvertToUByteArray(new[]
         {
         0, 7, 14, 9, 28, 27, 18, 21, 56, 63, 54, 49, 36, 35, 42, 45, 112, 119, 126, 121,
         108, 107, 98, 101, 72, 79, 70, 65, 84, 83, 90, 93, -32, -25, -18, -23, -4, -5,
@@ -91,50 +91,50 @@ namespace Catprinter.Utils
         57, 48, 55, 34, 37, 44, 43, 6, 1, 8, 15, 26, 29, 20, 19, -82, -87, -96, -89, -78,
         -75, -68, -69, -106, -111, -104, -97, -118, -115, -124, -125, -34, -39, -48, -41,
         -62, -59, -52, -53, -26, -31, -24, -17, -6, -3, -12, -13,
-    });
+        });
 
-        private static byte ChkSum(byte[] bArr, int i, int i2)
+        private static byte CalculateChecksumValue(byte[] bArr, int i, int i2)
         {
             byte b2 = 0;
             for (int i3 = i; i3 < i + i2; i3++)
             {
-                b2 = CHECKSUM_TABLE[(b2 ^ bArr[i3]) & 0xff];
+                b2 = CHECKSUM[(b2 ^ bArr[i3]) & 0xff];
             }
             return b2;
         }
 
-        public static byte[] CmdFeedPaper(int howMuch)
+        public static byte[] GenerateFeedPaperCommand(int amount)
         {
-            var bArr = new byte[9];
+            byte[] bArr = new byte[9];
             bArr[0] = 81;
             bArr[1] = 120;
             bArr[2] = ToUnsignedByte(-67);
             bArr[3] = 0;
             bArr[4] = 1;
             bArr[5] = 0;
-            bArr[6] = (byte)(howMuch & 0xff);
-            bArr[7] = ChkSum(bArr, 6, 1);
+            bArr[6] = (byte)(amount & 0xff);
+            bArr[7] = CalculateChecksumValue(bArr, 6, 1);
             bArr[8] = ToUnsignedByte(-1);
             return bArr;
         }
 
-        public static byte[] CmdSetEnergy(int val)
+        public static byte[] GenerateSetEnergyCommand(int val)
         {
-            var bArr = Bs(new[] { 81, 120, -81, 0, 2, 0, (val >> 8) & 0xff, val & 0xff, 0, 0xff });
-            bArr[8] = ChkSum(bArr, 6, 2);
+            byte[] bArr = ConvertToUByteArray(new[] { 81, 120, -81, 0, 2, 0, (val >> 8) & 0xff, val & 0xff, 0, 0xff });
+            bArr[8] = CalculateChecksumValue(bArr, 6, 2);
             return bArr;
         }
 
-        public static byte[] CmdApplyEnergy()
+        public static byte[] GenerateApplyEnergyCommand()
         {
-            var bArr = Bs(new[] { 81, 120, -66, 0, 1, 0, 1, 0, 0xff });
-            bArr[7] = ChkSum(bArr, 6, 1);
+            byte[] bArr = ConvertToUByteArray(new[] { 81, 120, -66, 0, 1, 0, 1, 0, 0xff });
+            bArr[7] = CalculateChecksumValue(bArr, 6, 1);
             return bArr;
         }
 
         private static List<int> EncodeRunLengthRepetition(int n, int val)
         {
-            var res = new List<int>();
+            List<int> res = new List<int>();
             while (n > 0x7f)
             {
                 res.Add(0x7f | (val << 7));
@@ -149,10 +149,10 @@ namespace Catprinter.Utils
 
         private static List<int> RunLengthEncode(IEnumerable<int> imgRow)
         {
-            var res = new List<int>();
+            List<int> res = new List<int>();
             int count = 0;
             int lastVal = -1;
-            foreach (var val in imgRow)
+            foreach (int val in imgRow)
             {
                 if (val == lastVal)
                 {
@@ -179,7 +179,7 @@ namespace Catprinter.Utils
                 return imgRow[chunkStart + bitIndex] != 0 ? 1 << bitIndex : 0;
             }
 
-            var res = new List<int>();
+            List<int> res = new List<int>();
             for (int chunkStart = 0; chunkStart < imgRow.Count; chunkStart += 8)
             {
                 int byteVal = 0;
@@ -192,45 +192,21 @@ namespace Catprinter.Utils
             return res;
         }
 
-        public static byte[] CmdPrintRow(IList<int> imgRow)
+        public static byte[] GeneratePrintRowCommand(IList<int> imgRow)
         {
-            var encodedImg = RunLengthEncode(imgRow);
+            List<int> encodedImg = RunLengthEncode(imgRow);
 
             if (encodedImg.Count > PRINT_WIDTH / 8)
             {
                 encodedImg = ByteEncode(imgRow);
-                var bArr = Bs(new[] { 81, 120, -94, 0, encodedImg.Count, 0 }.Concat(encodedImg).Concat(new[] { 0, 0xff }).ToArray());
-                bArr[bArr.Length - 2] = ChkSum(bArr, 6, encodedImg.Count);
+                byte[] bArr = ConvertToUByteArray(new[] { 81, 120, -94, 0, encodedImg.Count, 0 }.Concat(encodedImg).Concat(new[] { 0, 0xff }).ToArray());
+                bArr[bArr.Length - 2] = CalculateChecksumValue(bArr, 6, encodedImg.Count);
                 return bArr;
             }
 
-            var bArr2 = Bs(new[] { 81, 120, -65, 0, encodedImg.Count, 0 }.Concat(encodedImg).Concat(new[] { 0, 0xff }).ToArray());
-            bArr2[bArr2.Length - 2] = ChkSum(bArr2, 6, encodedImg.Count);
+            byte[] bArr2 = ConvertToUByteArray(new[] { 81, 120, -65, 0, encodedImg.Count, 0 }.Concat(encodedImg).Concat(new[] { 0, 0xff }).ToArray());
+            bArr2[bArr2.Length - 2] = CalculateChecksumValue(bArr2, 6, encodedImg.Count);
             return bArr2;
-        }
-
-        public static byte[] CmdsPrintImg(IEnumerable<IList<int>> img, int energy = 0xffff)
-        {
-            var data = new List<byte>();
-            data.AddRange(CMD_GET_DEV_STATE);
-            data.AddRange(CMD_SET_QUALITY_200_DPI);
-            data.AddRange(CmdSetEnergy(energy));
-            data.AddRange(CmdApplyEnergy());
-            data.AddRange(CMD_LATTICE_START);
-
-            foreach (var row in img)
-            {
-                data.AddRange(CmdPrintRow(row));
-            }
-
-            data.AddRange(CmdFeedPaper(25));
-            data.AddRange(CMD_SET_PAPER);
-            data.AddRange(CMD_SET_PAPER);
-            data.AddRange(CMD_SET_PAPER);
-            data.AddRange(CMD_LATTICE_END);
-            data.AddRange(CMD_GET_DEV_STATE);
-
-            return data.ToArray();
         }
     }
 }
